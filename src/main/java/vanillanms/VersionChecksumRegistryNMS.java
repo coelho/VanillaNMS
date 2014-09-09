88a3a90a905d171db6fe6e4440647fbae69a664b
coelho <me@iscoelho.com>
2014-09-08 20:44:09 -0700

    Oops. This checksum is for 1.8, not 1.7

--- src/main/java/vanillanms/VersionChecksumRegistryNMS.java
+++ src/main/java/vanillanms/VersionChecksumRegistryNMS.java
@@ -5,7 +5,7 @@ public class VersionChecksumRegistryNMS extends VersionChecksumRegistry {
 	public static final VersionChecksumRegistry instance = new VersionChecksumRegistryNMS();
 
 	private VersionChecksumRegistryNMS() {
-		super.submit("v17", "a028f00e678ee5c6aef0e29656dca091b5df11c7");
+		super.submit("v18", "a028f00e678ee5c6aef0e29656dca091b5df11c7");
 	}
 
 }
