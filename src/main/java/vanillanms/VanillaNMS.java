b93ccaa843e9325627bcea746adae14a25afc7c9
coelho <me@iscoelho.com>
2014-09-08 22:09:16 -0700

    Make our loaded message less annoying

--- src/main/java/vanillanms/VanillaNMS.java
+++ src/main/java/vanillanms/VanillaNMS.java
@@ -1,9 +1,13 @@
 package vanillanms;
 
 import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import vanillanms.util.StringUtils;
 
 public class VanillaNMS extends JavaPlugin {
 
@@ -14,6 +18,7 @@ public class VanillaNMS extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		Set<String> loadedVersions = new TreeSet<String>();
 		for(File file : super.getDataFolder().listFiles()) {
 			if(file.isDirectory()) {
 				continue;
@@ -22,13 +27,15 @@ public class VanillaNMS extends JavaPlugin {
 				continue;
 			}
 			try {
-				String version = VersionLoader.load(VersionChecksumRegistryNMS.instance, file);
-				super.getLogger().info("Loaded NMS: " + version);
+				loadedVersions.add(VersionLoader.load(VersionChecksumRegistryNMS.instance, file));
 			} catch(VersionLoaderNotFoundException exception) {
 				super.getLogger().warning(exception.getMessage());
 			} catch(Exception exception) {
 				super.getLogger().log(Level.SEVERE, "", exception);
 			}
+		}
+		if (!loadedVersions.isEmpty()) {
+			super.getLogger().info("Loaded NMS libraries: " + StringUtils.concat(loadedVersions, ", "));
 		}
 	}
 
